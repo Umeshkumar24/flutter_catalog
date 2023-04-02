3b2ab951b75d82a992155d9781ace648475bcdf0
Umeshkumar24 <dasumesh957@gmail.com>
2023-04-02 20:01:15 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  int days = 30;
-  String name = "Codepur";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Catalog App"),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of $name"),
-        ),
-      ),
-      drawer: const Drawer(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login" :(context) => LoginPage(),
+        "/home" :(context) => HomePage()
+      },
     );
   }
 }
